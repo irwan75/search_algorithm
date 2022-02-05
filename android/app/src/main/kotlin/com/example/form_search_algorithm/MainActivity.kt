d9f589a9554fc94754b36d0bff15be923bdbd200
irwan75 <irwan_ardyansah@transretail.co.id>
2022-02-05 07:07:19 +0700

    first initialize

---
+++ android/app/src/main/kotlin/com/example/form_search_algorithm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.form_search_algorithm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       child: Scaffold(
+         appBar: AppBar(
+           
+         ),
+         body: Container(),
+       ),
+    );
+  }
+}
